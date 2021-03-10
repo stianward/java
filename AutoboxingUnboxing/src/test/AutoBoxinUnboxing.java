a90d0eec590676d9d26f3d4dc78f08aceea9feb0
christian <stianward@gmail.com>
2021-03-10 11:47:53 -0500

    Autoboxing Unboxing java

---
+++ AutoboxingUnboxing/src/test/AutoBoxinUnboxing.java
@@ -0,0 +1,18 @@
+package test;
+public class AutoBoxinUnboxing {
+    public static void main(String[] args) {
+       
+        //autoboxing
+        /*envolver valor prmitivo a tipo object*/
+        Integer num1=1;
+        System.out.println("num1 = " + num1.toString());
+        
+        //unboxing
+        /* extraer el valor del objeto y lo convierte a tipo primtiv**/
+        
+        int num=num1;
+        System.out.println("num = " + num);
+        
+    }
+    
+}
