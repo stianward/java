c5cee43e9088270ce9468dcb47886476c1b54082
christian <stianward@gmail.com>
2021-03-10 12:38:35 -0500

    Modificadores de acceso en java

---
+++ ModifcadoresAcceso/src/paquete1/Clase1.java
@@ -0,0 +1,4 @@
+package paquete1;
+public class Clase1 {
+    public int valor=2;
+}
---
+++ ModifcadoresAcceso/src/test/test.java
@@ -0,0 +1,11 @@
+package test;
+
+import paquete1.Clase1;
+
+public class test {
+    public static void main(String[] args) {
+        Clase1 c=new Clase1();
+        System.out.println("c = " + c.valor);
+    }
+    
+}
