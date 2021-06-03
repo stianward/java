3382be24ae661c8e16164638a028f1c8cfe7f6d3
stianward <stianward@gmail.com>
2021-06-02 23:12:15 -0500

    Revert "EXPRESSION LANGUAJE"
    This reverts commit e3e8dd721fa8226297127c6108f795435f84adbd.

--- EjemploExpressionLenguaje/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.mycompany/EjemploExpressionLenguaje .
-call docker rm -f EjemploExpressionLenguaje
-call docker run -d -p 9080:9080 -p 9443:9443 --name EjemploExpressionLenguaje com.mycompany/EjemploExpressionLenguaje
